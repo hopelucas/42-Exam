3c42b5e86e789af71da1f30e43f681504bc621a6
Hope Lucas <hopelucas17@gmail.com>
2022-01-18 20:14:52 +1030

    exam prep

---
+++ Level 0/Inter/inter.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+
+int main(int argc, char *argv[])
+{
+    int i;
+    int j;
+
+    i = -1;
+    j = -1;
+
+    if (argc == 3)
+    {
+        while (argv[1][++i])
+        {
+            while (argv[2][++j])
+            {
+                if  (argv[1][i] != && argv[1][i] == argv[2][j])
+                {
+                    write(1, &argv[1][i], 1);
+                    argv[1][i] = 1;
+                }
+            }
+        }
+    }
+    write(1, "\n", 1);
+}
